9f52edc1814abf914ae0c76ee8de69fa05a5ad71
Julio Mendes <juliomendes160@hotmail.com>
2025-06-10 17:27:28 -0300

    Final

---
+++ .vscode/.bash
@@ -0,0 +1,8 @@
+bash="C:\Program Files\Git\bin"
+cmd="C:\WINDOWS\system32"
+powershell="C:\WINDOWS\System32\WindowsPowerShell\v1.0"
+code="C:\Program Files\Microsoft VS Code\bin"
+git="C:\Program Files\Git\mingw64\bin"
+node="\\Program Files\nodejs\v16.16.0\bin"
+PATH=$(/bin/cygpath -p "$bash;$cmd;$powershell;$code;$git;$node")
+[ -f "$workspaceFolder\setup.sh" ] && source "$workspaceFolder\setup.sh"
