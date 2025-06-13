f28470c8ef756fcc0fec7be889f0d88f98913c98
Julio Mendes <juliomendes160@hotmail.com>
2025-06-13 16:33:39 -0300

    Final commit

---
+++ .vscode/.bash
@@ -0,0 +1,9 @@
+echo projeto .bash
+bash="C:\Program Files\Git\bin"
+cmd="C:\WINDOWS\system32"
+powershell="C:\WINDOWS\System32\WindowsPowerShell\v1.0"
+code="C:\Program Files\Microsoft VS Code\bin"
+git="C:\Program Files\Git\mingw64\bin"
+node="\\Program Files\nodejs\v16.16.0\bin"
+[ -f "$workspaceFolder\\setup.sh" ] && source "$workspaceFolder\\setup.sh"
+PATH=$(/bin/cygpath -p "$bash;$cmd;$powershell;$code;$git;$node")
--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AngularComponent } from './angular/angular.component';
+import { PessoaComponent } from './pessoa/pessoa.component';
+import { LogradouroComponent } from './logradouro/logradouro.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+	{path:'', component: AngularComponent, pathMatch: 'full'},
+	{path:'pessoa', component: PessoaComponent, pathMatch: 'prefix'},
+	{path:'logradouro', component: LogradouroComponent, pathMatch: 'prefix'},
+	{path:'**', redirectTo:''},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
