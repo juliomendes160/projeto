9d5af549a326fce0c30a99e3774ef9421a79cda4
Julio Mendes <juliomendes160@hotmail.com>
2025-07-02 12:44:00 -0300

    chore: final commit

---
+++ .vscode/.bash
@@ -0,0 +1,6 @@
+bash="C:\Program Files\Git\bin"
+cmd="C:\WINDOWS\system32"
+powershell="C:\WINDOWS\System32\WindowsPowerShell\v1.0"
+code="C:\Program Files\Microsoft VS Code\bin"
+git="C:\Program Files\Git\mingw64\bin"
+PATH=$(/bin/cygpath -p "$bash;$cmd;$powershell;$code;$git;")
