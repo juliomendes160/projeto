1d21c4a4d74f65c6e906a3be9cf0fb1cfeee6d4f
Julio Mendes <juliomendes160@hotmail.com>
2025-07-08 11:26:04 -0300

    chore: final commit

---
+++ .vscode/.cmd
@@ -0,0 +1,7 @@
+@echo off
+set bash=C:\Program Files\Git\bin
+set cmd=C:\WINDOWS\system32
+set powershell=C:\WINDOWS\System32\WindowsPowerShell\v1.0
+set code=C:\Program Files\Microsoft VS Code\bin
+set git=C:\Program Files\Git\mingw64\bin
+set PATH=%bash%;%cmd%;%powershell%;%code%;%git%;
---
+++ setup.sh
@@ -0,0 +1,20 @@
+# chore: initial commit
+# chore: environment
+# feat: project
+# chore: dependency
+# chore: server
+# chore: final commit
+
+	git clone <repository-url>
+
+	git submodule add --force -b <branch-name> <branch-url> <folder-name>
+
+	git commit --allow-empty -m "<commit-message>"
+
+	git push --set-upstream origin <branch-name> </branch-name>
+
+	git push --force
+
+	git reset --hard
+
+	git clean -fd
